e17804736c470c2e8ef378fe2f9ee43dbbdbf3cc
yoseph007 <yos@sky.com>
2019-11-10 13:01:56 +0000

    Initial Commit

---
+++ hello.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if (filename == './') {filename = './index';}
+
+    filename = filename + ".html";
+
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");        
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    })
+}).listen(8080);
+
+console.log("Server lestening on port 8080....")
